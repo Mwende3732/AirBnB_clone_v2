493aca6607aacd7ca7e7c015b5f8f375eefb01f3
Mwende <muthianisharlinemwende@gmail.com>
2022-10-15 16:19:11 +0300

    cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
 starts a flask web application
-rt Flask
+"""
+from models import storage
+from flask import Flask
 from flask import render_template
 
-app = """
-from models import storage
-from flask impoFlask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
-"""
-    Displays a HTML page with a list of all stdef cities_by_states():
-    ates and related cities
+def cities_by_states():
+    """
+    Displays a HTML page with a list of all states and related cities
     """
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
